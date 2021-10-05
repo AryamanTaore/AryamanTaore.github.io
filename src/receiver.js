6dff96a5588e5bb12c04b5a8d72ed697057cf30a
AryamanTaore <aryamantaore@gmail.com>
2021-10-05 23:52:15 +1300

    updatedSVG

--- src/receiver.js
+++ src/receiver.js
@@ -20,12 +20,6 @@ class Receiver extends KeypadPeer {
     this.peer.on("error", this.onPeerError);
   }
 
-  // reconnect = () => {
-  //   this.peer.id = this.lastPeerId;
-  //   this.peer._lastServerId = this.lastPeerId;
-  //   this.peer.reconnect();
-  // }
-
   updateAlphabet = (alphabet) => {
     // Get an array of unique symbols
     const validAlphabet = this.checkAlphabet(alphabet);
